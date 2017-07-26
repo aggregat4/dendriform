482d97fa2d40fff064ffa4495e34bdb67ab65ada
Boris Terzic <boris@terzic.be>
2017-07-26 09:06:17 +0200

    First pass at rendering a tree with nested nodes

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import * as store from './store'
 import * as view from './view'
 
 const projector = maquette.createProjector()
-let STORE = {}
+const STORE = {}
 
 loadStore()
 
@@ -15,12 +15,15 @@ function getRequestedNodeId() {
 
 function loadStore() {
 	store.loadTree(getRequestedNodeId())
-		.then((tree) => { return STORE.tree = tree })
+		.then((tree) => { 
+			STORE.tree = tree
+			projector.scheduleRender()
+		})
 		.catch((reason) =>  {
 			console.log(`Error loading and rendering tree: ${reason}`)
 		})
 }
-
+ 
 // NEVER FORGET TO DEFER DOM INITIALISATION STUFF UNTIL THE DOM IS LOADED
 // YOU TWAT
 document.addEventListener('DOMContentLoaded', () => {
--- src/view.js
+++ src/view.js
@@ -2,12 +2,21 @@ import * as maquette from 'maquette'
 
 const h = maquette.h
 
-function render(tree) {
-  console.log(`got a tree: ${JSON.stringify(tree)}`)
-  return h('span', [
-    h('h2', ['This is the sub heading, really.']),
-    h('h3', ['Tree this'])
-  ])
+function render(node) {
+   // TODO potentially deal with non existant tree in renderer
+  console.log(`got a tree: ${JSON.stringify(node)}`)
+  function renderChildren(children) {
+    if (children && children.length > 0) {
+      return [h('div.children', children.map(c => render(c)))]
+    } else {
+      return []
+    }
+  }
+  return h('div.node', { id: node._id, 'data-rev': node._rev }, [
+      h('a', { href: `#node=${node._id}` }, '*'),
+      h('div.name', { contentEditable: 'true' }, node.name)
+    ].concat(renderChildren(node.children))
+  )
 }
 
 export function createTreeRenderer(tree) {
