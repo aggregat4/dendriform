7caa2411378e0f87f1845a372918e2ead1e95230
Boris Terzic <boris@terzic.be>
2018-07-04 09:12:43 +0200

    Renaming putNode to updateNode in repository

--- src/ts/repository/repository.ts
+++ src/ts/repository/repository.ts
@@ -2,7 +2,7 @@ import {RepositoryNode, LoadedTree, RelativeNodePosition} from '../domain/domain
 
 export interface Repository {
   createNode(id: string, name: string, content: string): Promise<RepositoryNode>
-  putNode(node: RepositoryNode): Promise<void>
+  updateNode(node: RepositoryNode): Promise<void>
   reparentNode(childId: string, parentId: string, position: RelativeNodePosition): Promise<void>
 
   getChildIds(nodeId: string): Promise<string[]>
