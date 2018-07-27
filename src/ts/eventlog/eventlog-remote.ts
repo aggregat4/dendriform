9de5fcce197e6268b7ae8f52c67b301466b75a51
Boris Terzic <boris@terzic.be>
2018-07-27 09:53:16 +0200

    Lintfix

--- src/ts/eventlog/eventlog-remote.ts
+++ src/ts/eventlog/eventlog-remote.ts
@@ -2,7 +2,7 @@
 
 // /**
 //  * A remote event log is a proxy to an actual server based instance of an event log.
-//  * It is only meant to synchronize events and thus only implements the EventLog 
+//  * It is only meant to synchronize events and thus only implements the EventLog
 //  * interface and not the QueryableEventLog interface.
 //  */
 // class RemoteEventLog implements DEventLog {
