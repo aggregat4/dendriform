6a12b59d71b76443a9e14a25656b69aa7916a134
Boris Terzic <boris@terzic.be>
2021-11-12 10:11:35 +0100

    Intermediate state as we move towards a new storage schema

--- src/ts/repository/repository-logandtreestorage.ts
+++ src/ts/repository/repository-logandtreestorage.ts
@@ -1,7 +1,7 @@
 import { RelativeNodePosition } from '../domain/domain'
-import { Subscription } from '../domain/lifecycle'
-import { DEventLog } from '../eventlog/eventlog-domain'
-import { IdbTreeStorage } from '../treepersistence/idb-treestorage'
+import { LifecycleAware, Subscription } from '../domain/lifecycle'
+import { DEvent, DEventLog } from '../eventlog/eventlog-domain'
+import { IdbTreeStorage } from '../storage/idb-treestorage'
 import { Predicate } from '../utils/util'
 import { LoadedTree, Repository, RepositoryNode } from './repository'
 
@@ -22,10 +22,31 @@ class NodeChangedSubscription implements Subscription {
 }
 
 // TODO: most of the real implementation (also subscribe to eventlog to update treeStorageStructure and notify subscribers)
-export class LogAndTreeStorageRepository implements Repository {
+export class LogAndTreeStorageRepository implements Repository, LifecycleAware {
   private changeSubscriptions: NodeChangedSubscription[] = []
+  private eventLogSubscription: Subscription = null
 
   constructor(readonly treeStorage: IdbTreeStorage, readonly eventLog: DEventLog) {}
+
+  async init(): Promise<void> {
+    this.eventLogSubscription = this.eventLog.subscribe({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      notify: this.eventLogListener.bind(this),
+      // TODO: verify whether we even want to differentiate between event originators , more generic would be not to but perhaps we can do some optimasation when treat local events differently
+      filter: (event) => event.originator !== this.eventLog.getPeerId(),
+    })
+  }
+
+  private eventLogListener(events: DEvent[]): void {
+    // TODO: implement
+  }
+
+  async deinit(): Promise<void> {
+    if (this.eventLogSubscription) {
+      this.eventLogSubscription.cancel()
+      this.eventLogSubscription = null
+    }
+  }
 
   async loadNode(nodeId: string, nodeFilter: Predicate<RepositoryNode>): Promise<RepositoryNode> {
     const node = await this.treeStorage.getNode(nodeId)
@@ -94,13 +115,5 @@ export class LogAndTreeStorageRepository implements Repository {
     if (subscriptionIndex >= 0) {
       this.changeSubscriptions.splice(subscriptionIndex, 1)
     }
-  }
-
-  async init(): Promise<void> {
-    throw new Error('Method not implemented.')
-  }
-
-  async deinit(): Promise<void> {
-    throw new Error('Method not implemented.')
   }
 }
