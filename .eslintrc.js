2b03e27d77b01ab0a376e97f83ee7388a3840574
Boris Terzic <boris@terzic.be>
2022-01-21 10:02:01 +0100

    Adding new eslint rules to catch floating promises and fixing some leftover issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   extends: [
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   rules: {
     'prettier/prettier': ['error'],
+    '@typescript-eslint/no-floating-promises': ['error'],
+    '@typescript-eslint/no-extra-semi': 'off',
   },
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
     ecmaVersion: '2018',
   },
-  plugins: ['prettier'],
+  plugins: ['prettier', '@typescript-eslint'],
 }
