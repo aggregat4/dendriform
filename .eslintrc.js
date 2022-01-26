d2f068b4dee250f18305d1faa3bb2b2ee6357e89
Boris Terzic <boris@terzic.be>
2022-01-26 10:27:57 +0100

    Fixing an eslint self referential problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'plugin:@typescript-eslint/recommended',
   ],
+  // we need to ignore this file itself or it will cause eslint problems :facepalm:, see https://stackoverflow.com/questions/63118405/how-to-fix-eslintrc-the-file-does-not-match-your-project-config
+  ignorePatterns: ['.eslintrc.js'],
+  // I think I need this for the @typescript-eslint rules, but I'm not sure
   parser: '@typescript-eslint/parser',
   rules: {
     'prettier/prettier': ['error'],
