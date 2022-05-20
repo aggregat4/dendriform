b23e7ed3862250d86c2c1db315a2d9464f9d7192
Boris Terzic <boris@terzic.be>
2022-05-20 10:05:26 +0200

    Removing --disable-web-security flag from chrome instances so as not to run into CORS problems

--- lib/puppeteer-utils.ts
+++ lib/puppeteer-utils.ts
@@ -4,15 +4,16 @@ export async function createBrowser(puppeteer) {
   return await puppeteer.launch({
     headless: true,
     ignoreHTTPSErrors: true,
-    args: ['--disable-web-security', '--allow-file-access-from-files'],
+    // '--disable-web-security',
+    args: ['--allow-file-access-from-files'],
   })
 }
 
 export function setupPageConsoleHandler(page: Page) {
   page.on('console', (msg) => {
-    if (msg.type() !== 'debug') {
-      console.log(`console ${msg.type()}:`, msg.text())
-    }
+    // if (msg.type() !== 'debug') {
+    console.log(`console ${msg.type()}:`, msg.text())
+    // }
     // if (msg.type() === 'error') {
     //   for (let i = 0; i < msg.args().length; i++) {
     //     console.log(msg.args()[i])
